88bcb017a3f94a08a54f4a38034b392ba566cd8a
oussama <abz1999abz@gmail.com>
2021-01-19 16:21:21 +0100

    last commit

--- Live-Sketch.py
+++ Live-Sketch.py
@@ -6,11 +6,19 @@ def sketch(img):
     imgGrayBlur = cv2.GaussianBlur(imgGray,(5,5),0)
     canny = cv2.Canny(imgGrayBlur,10,70)
     ret ,mask = cv2.threshold(canny,70,255,cv2.THRESH_BINARY_INV)
-    return ret
+    return mask
 
-image = cv2.imread('images/input.jpg')
-mask = sketch(image)
-cv2.imshow('Original', mask)
-cv2.waitKey()
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret ,frame = cap.read()
+    mask = sketch(frame)
+    cv2.imshow('Live Sketch', mask)
+    if cv2.waitKey(1) == 13:
+        break
+    
+cap.release()
 cv2.destroyAllWindows()
 
